184e8ab97524271828f4226c41b25d0d4fb01631
cwilliams <coreywllms9@gmail.com>
2020-02-12 16:04:50 -0500

    finished first test in personhandler

--- src/main/java/com/zipcodewilmington/PersonHandler.java
+++ src/main/java/com/zipcodewilmington/PersonHandler.java
@@ -11,27 +11,33 @@ public class PersonHandler {
     }
 
     public String whileLoop() {
-        String result = "";
+        String currentPerson = "";
         // create a `counter`
+        int counter = 0;
         // while `counter` is less than length of array
             // begin loop
+        while(counter<personArray.length) {
+            // use `counter` to identify the `current Person` in the array
+            // get `string Representation` of `currentPerson`
+            // append `stringRepresentation` to `result` variable
+    currentPerson += personArray[counter];
+    counter++;
 
-                // use `counter` to identify the `current Person` in the array
-                // get `string Representation` of `currentPerson`
-                // append `stringRepresentation` to `result` variable
-
-            // end loop
-        return result;
+        }    // end loop
+        return currentPerson;
     }
 
 
 
     public String forLoop() {
         String result = "";
+        String currentPerson = "";
         // identify initial value
         // identify terminal condition
         // identify increment
-
+for (int counter=0; counter<personArray.length; counter++){
+    result += personArray[counter];
+}
         // use the above clauses to declare for-loop signature
             // begin loop
                 // use `counter` to identify the `current Person` in the array
@@ -48,6 +54,7 @@ public class PersonHandler {
         String result = "";
         // identify array's type
         // identify array's variable-name
+
 
         // use the above discoveries to declare for-each-loop signature
             // begin loop
